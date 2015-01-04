121d5cb8a77b2cffe9864c8d9aa08e2dbc963dee
Shannow13 <mescle@et.esiea.fr>
2015-01-04 20:29:01 +0100

    Francisation des vues et ajout de commentaires

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  
+  #On définit le fait qu'un utilisateur puisse être administrateur
   def admin?
   	admin
   end
